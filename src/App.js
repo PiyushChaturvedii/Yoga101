da0aa23dc4fe9d560d24bca21485ed655d72619f
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2021-07-18 19:03:13 +0530

    "final"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './pages/About';
+import Header from './pages/Header';
+import Hero from './pages/Hero';
+import Gallery from './pages/Gallery';
+import Coach from './pages/Coach';
+import Contact from './pages/Contact';
+import WaterMark from 'watermark-component-for-react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="content-container">
+      <div class="contents">
+        <WaterMark content="Sample">
+
+        <Header />
+        <Hero />
+        <About />
+        <Gallery />
+        <Coach />
+        <Contact />
+        </WaterMark>
+    </div>
     </div>
   );
 }
